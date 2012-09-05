9f3dbf9570f7501bec316afe0d5b5a432394ebf6
Benjamin Feng <bfeng@enova.com>
2012-09-05 16:29:23 -0500

    Cleanup of accordion

--- app/assets/javascripts/macgyver/accordion.js
+++ app/assets/javascripts/macgyver/accordion.js
@@ -1,24 +1,20 @@
 $.fn.uiTransform = (function(){
   this.each(function(){
     var $instigator = $(this),
-        ui = $instigator.data('ui'),
-        defaults = {"accordion" : {"autoHeight" : false, "collapsible" : true, "active" : false}},
-        options = $.extend(true, {}, defaults[ui], $instigator.data('ui-options') || {}),
-        needs_ui_element = !(ui == 'accordion');
-    
-    var $ui = (needs_ui_element) ? $('<div id="' + $instigator.attr('id').replace(/_/g,'-') + '-ui-' + ui + '"></div>').insertAfter($instigator) : $instigator;
-    
+        defaults = {"autoHeight" : false, "collapsible" : true, "active" : false},
+        options = $.extend(true, {}, defaults, $instigator.data('ui-options') || {});
+
     var hash = window.location.hash || undefined;
     var $menu = (options.menu) ? $(options.menu) : undefined;
     if (hash) {
       var active_index = -1;
-      var $search = ($menu) ? $menu : $ui;
+      var $search = ($menu) ? $menu : $instigator;
       $search.find('a').each(function(index, anchorEl){
         if ($(anchorEl).attr('href') == hash) { active_index = index; return false; }
       });
       if (active_index > -1) { options.active = active_index; }
     }
-    
+
     if ($menu) {
       $menu.click(function(event) {
         var toggle_index = -1;
@@ -31,11 +27,11 @@ $.fn.uiTransform = (function(){
             };
           });
         }
-        if (toggle_index > -1) { $ui.accordion("activate", toggle_index); }
+        if (toggle_index > -1) { $instigator.accordion("activate", toggle_index); }
       });
     }
 
-    $ui.accordion(options);
+    $instigator.accordion(options);
   });
   return this;
 });
