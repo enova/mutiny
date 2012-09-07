2f35f3aa6d68362691f1b6f825d746d8db8677c2
Benjamin Feng <bfeng@enova.com>
2012-09-07 11:05:24 -0500

    Added example index for great justice!

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,2 +1,24 @@
 class ExamplesController < ApplicationController
+  def index
+    render :inline => <<-END
+    <ul>
+      <% examples.each do |example| %>
+        <li><%= link_to example, :action => example %></li>
+      <% end %>
+    </ul>
+    END
+  end
+
+  private
+
+  def examples
+    examples = Set.new
+    view_paths.each do |path|
+      Dir.chdir(File.join(path, controller_name)) do
+        examples.merge Dir.glob('*').map{|f| f.sub(/\..*/, '')}
+      end
+    end
+    examples
+  end
+  helper_method :examples
 end
