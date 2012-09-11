fb4fafedbebf9337913abcfe8afae3516c4db252
Benjamin Feng <bfeng@enova.com>
2012-09-11 15:10:38 -0500

    Replacement text for un-encoding JS.  Rather hacky...

--- app/assets/javascripts/examples.js
+++ app/assets/javascripts/examples.js
@@ -1,5 +1,7 @@
 $('article').each(function(i, e) {
   var $e = $(e);
   var $code = $('<code></code>').insertAfter($e.find('h3'));
-  $code.text($e.find('.example').html());
+  /* .html() pulls out the code of the parsed Javascript.  This can cause the
+     browser to re-encode ' => " and " => &quot; */
+  $code.text($e.find('.example').html().replace(/"/g, "'").replace(/&quot;/g, '"'));
 });
