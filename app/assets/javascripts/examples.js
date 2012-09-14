60b80f40fc3f0e2926b633f783fd571473b84a18
Benjamin Feng <bfeng@enova.com>
2012-09-14 11:27:52 -0500

    Updated barebones to be more barebones

--- app/assets/javascripts/examples.js
+++ app/assets/javascripts/examples.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('article').each(function(i, e) {
     var $e = $(e);
-    var $code = $('<code></code>').insertAfter($e.find('h3'));
+    var $code = $('<code></code>').insertAfter($e.find('> h3'));
     /* .html() pulls out the code of the parsed Javascript.  This can cause the
        browser to re-encode ' => " and " => &quot; */
     $code.text($e.find('.example').html().replace(/"/g, "'").replace(/&quot;/g, '"'));
