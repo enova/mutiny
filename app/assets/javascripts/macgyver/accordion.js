f15d8f43dda64a094e064e17dbd632780244933c
Benjamin Feng <bfeng@enova.com>
2012-09-14 10:52:46 -0500

    Refactored accordion

--- app/assets/javascripts/macgyver/accordion.js
+++ app/assets/javascripts/macgyver/accordion.js
@@ -1,32 +1,27 @@
 var Macgyver = function(macgyver, $, undefined) {
   macgyver.accordion = {
     'defaults': {'autoHeight': false, 'collapsible': true, 'active': false},
+    '_hrefIndex': function($search, href) {
+      var active_index = -1;
+      $search.find('a').each(function(index, anchorEl){
+        if ($(anchorEl).attr('href') == href) { active_index = index; return false; }
+      });
+      return active_index;
+    },
     'init': function($instigator, options){
       options = $.extend({}, this.defaults, options);
 
       var hash = window.location.hash || undefined;
       var $menu = (options['menu']) ? $(options['menu']) : undefined;
       if (hash) {
-        var active_index = -1;
-        var $search = ($menu) ? $menu : $instigator;
-        $search.find('a').each(function(index, anchorEl){
-          if ($(anchorEl).attr('href') == hash) { active_index = index; return false; }
-        });
+        var active_index = this._hrefIndex($menu || $instigator, hash);
         if (active_index > -1) { options['active'] = active_index; }
       }
 
       if ($menu) {
-        $menu.click(function(event) {
-          var toggle_index = -1;
-          var $target = $(event.target);
-          if ($target.is("a")) {
-            $menu.find('a').each(function(index, anchorEl){
-              if ($(anchorEl).attr('href') == $target.attr('href')) {
-                toggle_index = index;
-                return false;
-              };
-            });
-          }
+        var self = this;
+        $menu.find('a').click(function(event) {
+          var toggle_index = self._hrefIndex($menu, $(event.target).attr('href'));
           if (toggle_index > -1) { $instigator.accordion("activate", toggle_index); }
         });
       }
