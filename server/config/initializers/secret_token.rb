cf136aad277cf57fd0ae3007a99929328095617e
fengb <benjamin.feng@gmail.com>
2012-09-21 21:16:01 -0500

    Die Rails die!

--- server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mutiny::Application.config.secret_token = '348028f2453f7840bdc886ae006491c59c63035f8efe98636808447364f27ad04c088d8d14139d36104b5d12c107c2cab50648af66bc687949f7cc8a1dc820fd'
