c06874b7dfdf64a9a84c7fe076fc8ac9fe4976b9
fengb <benjamin.feng@gmail.com>
2012-09-23 19:06:57 -0500

    Added build output

--- grunt.js
+++ grunt.js
@@ -1,8 +1,27 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    meta: {
+      banner: '/*! Mutiny - https://github.com/enova/mutiny */'
+    },
+
+    concat: {
+      dist: {
+        src: ['<banner>', 'src/**/*.js'],
+        dest: 'dist/mutiny.js'
+      }
+    },
+
+    min: {
+      dist: {
+        src: ['<banner>', 'dist/mutiny.js'],
+        dest: 'dist/mutiny.min.js'
+      }
+    },
+
     lint: {
       all: ['grunt.js', 'src/**/*.js', 'spec/**/*.js']
     },
+
     jshint: {
       options: {
         browser: true
