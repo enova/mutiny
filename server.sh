e326121aa5acc3c4aba8d02e003ee8bb28e73f93
Benjamin Feng <bfeng@enova.com>
2012-09-28 10:28:45 -0500

    Updated server to work with Python 3

--- server.sh
+++ server.sh
@@ -3,4 +3,16 @@
 PORT=${1:-5100}
 
 cd `dirname "${BASH_SOURCE[0]}"`
-exec python -m SimpleHTTPServer $PORT
+
+exist() {
+  command -v $1 >/dev/null 2>&1
+}
+
+if exist python3; then
+  exec python3 -m http.server $PORT
+elif exist python; then
+  exec python -m SimpleHTTPServer $PORT
+else
+  echo 'No supported server found.' >&2
+  exit 1
+fi
