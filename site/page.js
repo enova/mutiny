5fecd788244492f537d7855cb7f9fdc7d1c134eb
fengb <benjamin.feng@gmail.com>
2012-09-30 13:16:19 -0500

    Refactored page.js

--- site/page.js
+++ site/page.js
@@ -1,11 +1,10 @@
-var Page = function($) {
-  var helpers = ['_jquery'];
-  var widgets = ['accordion', 'slider', 'toggler'];
-  var files = helpers.concat(widgets);
+var Page = function(page, $) {
+  page.helpers = ['_jquery'];
+  page.widgets = ['accordion', 'slider', 'toggler'];
+  page.files = page.helpers.concat(page.widgets);
 
   /* Manual DOM manipulation because jQuery versions swallow errors */
-  var include = function(paths, transform) {
-    transform = transform || '%s';
+  page.include = function(paths, transform) {
     for(var i=0; i < paths.length; i++) {
       var script = document.createElement('script');
       script.src = transform.replace('%s', paths[i]);
@@ -13,38 +12,7 @@ var Page = function($) {
     }
   };
 
-  include(files, '/src/%s.js');
+  page.include(page.files, '/src/%s.js');
 
-  $.each(widgets, function(i, widget) {
-    var $e = $('<a href="#%s">%s</a>'.replace(/%s/g, widget));
-    $e.appendTo('#examples')
-      .click(function() {
-        /* Replace with Mutiny.tabber */
-        $('#examples a').removeClass('active');
-        $e.addClass('active');
-
-        $('#main').load('/examples/' + widget + '.html', function() {
-          $('#main section').each(function(i, e) {
-            var $e = $(e);
-            var $pre = $('<pre></pre>').insertBefore($e.find('.example'));
-            var $code = $('<code data-language="html"></code>').appendTo($pre);
-            /* .html() pulls out the code of the parsed Javascript.  This can cause the
-            browser to re-encode ' => " and " => &quot; */
-            $code.text($e.find('.example').html().replace(/"/g,      "'")
-                                                 .replace(/&quot;/g, '"')
-                                                 .replace(/&gt;/g,   '>'));
-          });
-
-          Rainbow.color();
-          $('#main *').mutiny();
-        });
-        return false;
-      });
-  });
-
-  return {
-    'loadSpecs': function() {
-      include(files, '/spec/%s_spec.js');
-    }
-  };
-}(jQuery);
+  return page;
+}({}, jQuery);
