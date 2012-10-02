e0a70c211c040e56df0a6f812ae97bd3a24d4b4d
fengb <benjamin.feng@gmail.com>
2012-10-01 22:53:03 -0500

    Updated server script to allow for specific server execution

--- server.sh
+++ server.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# The 51st century is when it all changes.
-PORT=${1:-5100}
+if [ $1 -gt 0 ] &>/dev/null; then
+  PORT=$1
+else
+  # The 51st century is when it all changes.
+  PORT=5100
+  COMMAND=$1
+fi
 
 cd `dirname "${BASH_SOURCE[0]}"`
 
-exist() {
-  command -v $1 >/dev/null 2>&1
+should_run() {
+  [ "$COMMAND" = "$1" ] || ( [ -z $COMMAND ] && command -v $1 >/dev/null 2>&1 )
 }
 
-if exist grunt; then
+if should_run grunt; then
   exec grunt set_config:server.port:$PORT server wait
-elif exist python3; then
+elif should_run python3; then
   exec python3 -m http.server $PORT
-elif exist python; then
+elif should_run python; then
   exec python -m SimpleHTTPServer $PORT
 else
-  echo 'No web server found.' >&2
+  echo 'No compatible web server found.' >&2
   exit 1
 fi
