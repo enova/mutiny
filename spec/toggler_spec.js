0a77f2fd302e64299d9a2f4e80b1fe650972aa83
Aleksandra Bondzic <abondzic@enova.com>
2013-01-11 10:56:46 -0600

    Added more tests for toggler and updated jasmine-jquery to correct version

--- spec/toggler_spec.js
+++ spec/toggler_spec.js
@@ -6,8 +6,20 @@ describe('Mutiny.toggler', function() {
 
 	it('triggers Bare Bones toggler', function() {
 		expect($('#bare-bones')).toBeVisible();
-		$('#bare_bones_toggler').click();
+		$('#bare-bones-toggler').click();
 		expect($('#bare-bones')).toBeHidden();
+	});
+
+	it('triggers Class Name toggler', function() {
+		expect($('#class-name')).not.toHaveClass('active');
+		$('#class-name-toggler').click();
+		expect($('#class-name')).toHaveClass('active');
+	});
+
+	it('triggers Style Changes toggler', function() {
+		expect($('#style-changes')).not.toHaveCss({'font-style':'italic'});
+		$('#style-changes-toggler').click();
+		expect($('#style-changes')).toHaveCss({'font-style':'italic'});
 	});
 
 	it('triggers Radio Button toggler', function() {
