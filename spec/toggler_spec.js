86907a69bdd618b7403e67935ebeb8d66e371cf0
fengb <benjamin.feng@gmail.com>
2013-01-21 17:18:40 -0600

    Tab removal

--- spec/toggler_spec.js
+++ spec/toggler_spec.js
@@ -1,30 +1,30 @@
 describe('Mutiny.toggler', function() {
-	beforeEach(function(){
-		loadFixtures('toggler.html');
-		Mutiny.init();
-	});
+  beforeEach(function(){
+    loadFixtures('toggler.html');
+    Mutiny.init();
+  });
 
-	it('triggers Bare Bones toggler', function() {
-		expect($('#bare-bones')).toBeVisible();
-		$('#bare-bones-toggler').click();
-		expect($('#bare-bones')).toBeHidden();
-	});
+  it('triggers Bare Bones toggler', function() {
+    expect($('#bare-bones')).toBeVisible();
+    $('#bare-bones-toggler').click();
+    expect($('#bare-bones')).toBeHidden();
+  });
 
-	it('triggers Class Name toggler', function() {
-		expect($('#class-name')).not.toHaveClass('active');
-		$('#class-name-toggler').click();
-		expect($('#class-name')).toHaveClass('active');
-	});
+  it('triggers Class Name toggler', function() {
+    expect($('#class-name')).not.toHaveClass('active');
+    $('#class-name-toggler').click();
+    expect($('#class-name')).toHaveClass('active');
+  });
 
-	it('triggers Style Changes toggler', function() {
-		expect($('#style-changes')).not.toHaveCss({'font-style':'italic'});
-		$('#style-changes-toggler').click();
-		expect($('#style-changes')).toHaveCss({'font-style':'italic'});
-	});
+  it('triggers Style Changes toggler', function() {
+    expect($('#style-changes')).not.toHaveCss({'font-style':'italic'});
+    $('#style-changes-toggler').click();
+    expect($('#style-changes')).toHaveCss({'font-style':'italic'});
+  });
 
-	it('triggers Radio Button toggler', function() {
-		expect($('#option_group')).toBeHidden();
-		$('#choice2').click();
-		expect($('#option_group')).toBeVisible();
-	});
+  it('triggers Radio Button toggler', function() {
+    expect($('#option_group')).toBeHidden();
+    $('#choice2').click();
+    expect($('#option_group')).toBeVisible();
+  });
 });
