b19976549980f95ff6a3dbaedbc65500d600410c
fengb <benjamin.feng@gmail.com>
2013-01-25 21:16:39 -0600

    Renamed directive to widget for consistent verbiage

--- spec/_jquery_spec.js
+++ spec/_jquery_spec.js
@@ -1,92 +1,92 @@
 describe('$(element).mutiny()', function() {
   beforeEach(function() {
-    Mutiny.directive = {
+    Mutiny.widget = {
       'init': function(){}
     };
-    spyOn(Mutiny.directive, 'init');
+    spyOn(Mutiny.widget, 'init');
   });
 
   afterEach(function() {
-    delete Mutiny.directive;
+    delete Mutiny.widget;
   });
 
-  describe("<div data-mutiny='directive' />", function() {
+  describe("<div data-mutiny='widget' />", function() {
     beforeEach(function() {
-      this.el = $("<div data-mutiny='directive' />");
+      this.el = $("<div data-mutiny='widget' />");
     });
 
     it("invokes with empty options", function() {
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {});
     });
 
     it("invokes with defaults", function() {
-      Mutiny.directive.defaults = {'default': 'option'};
+      Mutiny.widget.defaults = {'default': 'option'};
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'default': 'option'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'default': 'option'});
     });
   });
 
-  describe('<div data-mutiny=\'{"directive": {"key": "value"}}\' />', function() {
+  describe('<div data-mutiny=\'{"widget": {"key": "value"}}\' />', function() {
     beforeEach(function() {
-      this.el = $('<div data-mutiny=\'{"directive": {"key": "value"}}\' />');
+      this.el = $('<div data-mutiny=\'{"widget": {"key": "value"}}\' />');
     });
 
     it("invokes with options", function() {
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'key': 'value'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'key': 'value'});
     });
 
     it("invokes with defaults merged with options", function() {
-      Mutiny.directive.defaults = {'default': 'option'};
+      Mutiny.widget.defaults = {'default': 'option'};
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'default': 'option', 'key': 'value'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'default': 'option', 'key': 'value'});
     });
 
     it("invokes with overridden defaults", function() {
-      Mutiny.directive.defaults = {'key': 'default'};
+      Mutiny.widget.defaults = {'key': 'default'};
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'key': 'value'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'key': 'value'});
     });
   });
 
-  describe('<div data-mutiny=\'{"directive": "string"}\' />', function() {
+  describe('<div data-mutiny=\'{"widget": "string"}\' />', function() {
     beforeEach(function() {
-      Mutiny.directive.string_arg = 'objectize';
-      this.el = $('<div data-mutiny=\'{"directive": "string"}\' />');
+      Mutiny.widget.string_arg = 'objectize';
+      this.el = $('<div data-mutiny=\'{"widget": "string"}\' />');
     });
 
-    it("converts options into {Mutiny.directive.string_arg: 'string'}", function() {
+    it("converts options into {Mutiny.widget.string_arg: 'string'}", function() {
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'objectize': 'string'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'objectize': 'string'});
     });
 
     it("merges defaults with options", function() {
-      Mutiny.directive.defaults = {'default': 'option'};
+      Mutiny.widget.defaults = {'default': 'option'};
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'default': 'option', 'objectize': 'string'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'default': 'option', 'objectize': 'string'});
     });
 
     it("overrides defaults with options", function() {
-      Mutiny.directive.defaults = {'objectize': 'default'};
+      Mutiny.widget.defaults = {'objectize': 'default'};
       this.el.mutiny();
-      expect(Mutiny.directive.init).wasCalledWith($(this.el[0]), {'objectize': 'string'});
+      expect(Mutiny.widget.init).wasCalledWith($(this.el[0]), {'objectize': 'string'});
     });
   });
 
-  describe("<div data-custom='directive'>", function() {
+  describe("<div data-custom='widget'>", function() {
     beforeEach(function() {
-      this.el = $("<div data-custom='directive' />");
+      this.el = $("<div data-custom='widget' />");
     });
 
     it("triggers using argument", function() {
       this.el.mutiny('custom');
-      expect(Mutiny.directive.init).wasCalled();
+      expect(Mutiny.widget.init).wasCalled();
     });
   });
 
   describe("exceptions", function() {
-    it("<div data-mutiny='nonexistent'> throws nonexistent directive", function() {
+    it("<div data-mutiny='nonexistent'> throws nonexistent widget", function() {
       var el = $("<div data-mutiny='nonexistent' />");
       expect(function() {
         el.mutiny();
