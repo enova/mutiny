e34d71b619fa336de17938946ed2ad4bde1270fc
fengb <benjamin.feng@gmail.com>
2013-01-25 21:35:30 -0600

    Refactored file loading

--- site/page.js
+++ site/page.js
@@ -1,12 +1,19 @@
 var Page = function(page, $) {
   page.helpers = ['_jquery'];
   page.widgets = ['accordion', 'datepicker', 'slider', 'toggler'];
-  page.files = page.helpers.concat(page.widgets);
 
-  /* Manual document.write to force blocked loading */
-  page.include = function(paths, transform) {
-    for(var i=0; i < paths.length; i++) {
-      document.write('<script src="' + transform.replace('%s', paths[i]) + '"><\/script>');
+  page.subPaths = page.helpers.concat(page.widgets);
+
+  page.srcFiles = $.map(page.subPaths, function(e){return 'src/'+e+'.js'});
+  page.specFiles = $.map(page.subPaths, function(e){return 'spec/'+e+'_spec.js'});
+
+  page.include = function() {
+    for(var i=0; i < arguments.length; i++) {
+      var arrayFiles = [].concat(arguments[i]);
+      for(var j=0; j < arrayFiles.length; j++) {
+        /* document.write to force blocked loading */
+        document.write('<script src="' + arrayFiles[j] + '"><\/script>');
+      }
     }
   };
 
