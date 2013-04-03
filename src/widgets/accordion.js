1d091d2f89007c3e7427c18f9fdd57acf7ccf028
fengb <benjamin.feng@gmail.com>
2013-04-03 15:19:15 -0500

    Updated menu to use hashchange instead of weird indexing

--- src/widgets/accordion.js
+++ src/widgets/accordion.js
@@ -1,28 +1,17 @@
 Mutiny.accordion = {
   'defaults': {'autoHeight': false, 'collapsible': true, 'active': false},
-  '_hrefIndex': function($search, href) {
-    var active_index = -1;
-    $search.find('a').each(function(index, anchorEl){
-      if ($(anchorEl).attr('href') == href) { active_index = index; return false; }
-    });
-    return active_index;
-  },
   'init': function($instigator, options){
-    var hash = window.location.hash || undefined;
-    var $menu = (options.menu) ? $(options.menu) : undefined;
-    if (hash) {
-      var active_index = this._hrefIndex($menu || $instigator, hash);
-      if (active_index > -1) { options.active = active_index; }
+    var hash = window.location.hash;
+    if(hash && $instigator.find(hash).length > 0) {
+      options.active = hash;
     }
-
-    if ($menu) {
-      var self = this;
-      $menu.find('a').click(function(event) {
-        var toggle_index = self._hrefIndex($menu, $(event.target).attr('href'));
-        if (toggle_index > -1) { $instigator.accordion("activate", toggle_index); }
-      });
-    }
-
     $instigator.accordion(options);
+
+    $(window).on('hashchange', function(event) {
+      var hash = window.location.hash;
+      if(hash && $instigator.find(hash).length > 0) {
+        $instigator.accordion('activate', hash);
+      }
+    });
   }
 };
