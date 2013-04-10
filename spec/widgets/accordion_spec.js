2934549ae65418c6042ca3f6700b046e6cb16bc7
fengb <benjamin.feng@gmail.com>
2013-04-10 12:24:46 -0500

    Added basic accordion spec

--- spec/widgets/accordion_spec.js
+++ spec/widgets/accordion_spec.js
@@ -1,1 +1,14 @@
-/* Add tests */
+describe('Mutiny.accordion', function() {
+  beforeEach(function(){
+    loadFixtures('accordion.html');
+    Mutiny.init();
+  });
+
+  it('triggers Bare Bones accordion', function() {
+    expect($('#bare-bones p:eq(0)')).toBeHidden();
+    expect($('#bare-bones p:eq(1)')).toBeHidden();
+    $('#bare-bones h2:eq(1)').click();
+    expect($('#bare-bones p:eq(0)')).toBeHidden();
+    expect($('#bare-bones p:eq(1)')).toBeVisible();
+  });
+});
