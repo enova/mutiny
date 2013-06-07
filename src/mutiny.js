9679c7b95af1e0b7053aca279428c2120a56f8e2
fengb <benjamin.feng@gmail.com>
2013-06-07 02:06:57 -0500

    Refactored inner functions outwards

--- src/mutiny.js
+++ src/mutiny.js
@@ -14,46 +14,19 @@ var Mutiny = window.Mutiny = {
   init: function($es, namespace) {
     namespace = namespace || 'mutiny';
 
-    var filter = function($es, arg) {
-      return $es ? $es.filter(arg) : $(arg);
-    };
-
-    var mutiny_call = function($instigator, widget_name, instance_options) {
-      /* Deprecated: Mutiny.<widget_name> should be Mutiny.widgets.<widget_name> */
-      var widget = Mutiny.widgets[widget_name] || Mutiny[widget_name];
-      if(widget === undefined) {
-        throw '"' + widget_name + '" not found';
-      }
-
-      var options = $.extend({}, widget.defaults);
-      if(isString(instance_options)) {
-        /* Deprecated: <widget>.string_arg should be <widget>.stringArg */
-        if(widget.string_arg) {
-          options[widget.string_arg] = instance_options;
-        } else if(widget.stringArg) {
-          options[widget.stringArg] = instance_options;
-        } else {
-          throw '"' + widget_name + '" cannot parse "' + instance_options + '"';
-        }
-      } else {
-        $.extend(options, instance_options);
-      }
-      widget.init($instigator, options);
-    };
-
+    /* Deprecated.  data-mutiny="widget" should be data-mutiny-widget="" */
     filter($es, '[data-' + namespace + ']').each(function(i, e) {
       var $e = $(e);
       var data = $e.data();
-      /* Deprecated.  data-mutiny="widget" should be data-mutiny-widget="" */
       if(namespace in data) {
         var directives = data[namespace];
         if(isString(directives)) {
           /* data-mutiny='slider' */
-          mutiny_call($e, directives, {});
+          mutinyCall($e, directives, {});
         } else if(typeof data === 'object') {
           /* data-mutiny='{"slider": {"some": "options"}}' */
           for(var directive in directives) {
-            mutiny_call($e, directive, directives[directive]);
+            mutinyCall($e, directive, directives[directive]);
           }
         } else {
           throw 'Unsupported data';
@@ -61,25 +34,53 @@ var Mutiny = window.Mutiny = {
       }
     });
 
-    var capitalize = function(string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    };
-
     var queries = [];
     for(var name in Mutiny.widgets) {
       queries.push('[data-' + namespace + '-' + name + ']');
     }
-    var $widgetable = filter($es, queries.join(','));
-    for(var i=0; i < $widgetable.length; i++) {
-      var $e = $($widgetable[i]);
+    var $needWidgets = filter($es, queries.join(','));
+    for(var i=0; i < $needWidgets.length; i++) {
+      var $e = $($needWidgets[i]);
       var data = $e.data();
       for(var key in data) {
         if(key.indexOf(namespace) === 0) {
-          var widget = key.replace(namespace, '').toLowerCase();
-          var options = data[key];
-          mutiny_call($e, widget, options || {});
+          var widget = lowerCaseFirst(key.replace(namespace, ''));
+          mutinyCall($e, widget, data[key]);
         }
       }
     }
   }
+};
+
+var filter = function($es, arg) {
+  return $es ? $es.filter(arg) : $(arg);
+};
+
+var mutinyCall = function($instigator, widget_name, options) {
+  /* Deprecated: Mutiny.<widget_name> should be Mutiny.widgets.<widget_name> */
+  var widget = Mutiny.widgets[widget_name] || Mutiny[widget_name];
+  if(widget === undefined) {
+    throw '"' + widget_name + '" not found';
+  }
+
+  var instanceOptions = {};
+  if(!options) {
+  } else if(isString(options)) {
+    /* Deprecated: <widget>.string_arg should be <widget>.stringArg */
+    if(widget.string_arg) {
+      instanceOptions[widget.string_arg] = options;
+    } else if(widget.stringArg) {
+      instanceOptions[widget.stringArg] = options;
+    } else {
+      throw '"' + widget_name + '" cannot parse "' + options + '"';
+    }
+  } else {
+    instanceOptions = options;
+  }
+
+  widget.init($instigator, $.extend({}, widget.defaults, instanceOptions));
+};
+
+var lowerCaseFirst = function(string) {
+  return string.charAt(0).toLowerCase() + string.slice(1);
 };
