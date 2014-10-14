b453ec57fb1497412c757c8468b07ebe62bb7fac
fengb <contact@fengb.info>
2014-10-13 20:41:54 -0500

    Include vendors

--- site/page.js
+++ site/page.js
@@ -1,24 +1,30 @@
 var Page = function(page) {
+  page.vendor = ['jquery', 'jquery-ui'];
   page.core = ['core'];
   page.widgets = [
     'jq/toggler',
     'jqui/accordion', 'jqui/datepicker', 'jqui/slider'
   ];
 
-  var mapFormat = function(arr, format) {
-    return $.map(arr, function(e) { return format.replace('%s', e); });
+  function mapFormat(arr, format) {
+    var ret = [];
+    for(var i=0; i < arr.length; i++){
+      ret.push(format.replace('%s', arr[i]));
+    }
+    return ret;
   };
 
   page.subPaths = page.core.concat(page.widgets);
-  page.src = mapFormat(page.subPaths, 'src/%s.js');
+  page.src = mapFormat(page.vendor, 'vendor/%s.js').concat(
+             mapFormat(page.subPaths, 'src/%s.js'));
   page.spec = mapFormat(page.subPaths, 'spec/unit/%s-spec.js').concat(
               mapFormat(page.subPaths, 'spec/func/%s-spec.js'));
 
   page.include = function(files) {
-    $.each(files, function(i, file) {
+    for(var i=0; i < files.length; i++){
       /* document.write to force blocked loading */
-      document.write('<script src="' + file + '"></script>');
-    });
+      document.write('<script src="' + files[i] + '"></script>');
+    }
   };
 
   return page;
