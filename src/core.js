471095efd4c24a264e8bd90c36645c93a9a12315
fengb <contact@fengb.info>
2014-10-13 20:16:22 -0500

    Refactored initWidget

--- src/core.js
+++ src/core.js
@@ -5,6 +5,40 @@ var Mutiny = window.Mutiny = {
 
   widgets: {},
 
+  init: function(els, namespace) {
+    namespace = namespace || 'mutiny';
+    var name;
+
+    if(!els) {
+      var queries = [];
+      for(name in Mutiny.widgets) {
+        if(Mutiny.widgets.hasOwnProperty(name)) {
+          queries.push(Mutiny.util.format('[data-{0}-{1}]', namespace, Mutiny.util.dasherize(name)));
+        }
+      }
+      els = document.querySelectorAll(queries.join(','));
+    } else if(!Mutiny.util.isArray(els)) {
+      els = [els];
+    }
+
+    var processed = Mutiny.util.format('data-{0}', namespace);
+    for(var i=0; i < els.length; i++) {
+      var el = els[i];
+      if(el.hasAttribute(processed)){
+        continue;
+      }
+
+      el.setAttribute(processed, 'processed');
+
+      for(name in Mutiny.widgets) {
+        var attr = Mutiny.util.format('data-{0}-{1}', namespace, Mutiny.util.dasherize(name));
+        if(el.hasAttribute(attr)){
+          Mutiny.util.initWidget(el, name, el.getAttribute(attr));
+        }
+      }
+    }
+  },
+
   util: {
     onReady: function(fn){
       if(document.readyState === 'complete'){
@@ -55,37 +89,27 @@ var Mutiny = window.Mutiny = {
 
     isArray: function(obj){
       return obj.length !== undefined;
-    }
-  },
+    },
 
-  init: function(els, namespace) {
-    namespace = namespace || 'mutiny';
-    var name;
+    initWidget: function(instigator, widgetName, instanceOptions) {
+      var widget = Mutiny.widgets[widgetName];
+      if(widget === undefined) {
+        throw Mutiny.util.format('"Mutiny.widget.{0}" not found', widgetName);
+      }
 
-    if(!els) {
-      var queries = [];
-      for(name in Mutiny.widgets) {
-        if(Mutiny.widgets.hasOwnProperty(name)) {
-          queries.push(Mutiny.util.format('[data-{0}-{1}]', namespace, Mutiny.util.dasherize(name)));
+      try {
+        instanceOptions = instanceOptions ? JSON.parse(instanceOptions) : {};
+      } catch(e) {
+        e.message = Mutiny.util.format('"Mutiny.widget.{0}" cannot parse "{1}"', widgetName, instanceOptions);
+        throw e;
+      }
+
+      for(var key in widget.defaults) {
+        if(widget.defaults.hasOwnProperty(key) && !instanceOptions.hasOwnProperty(key)) {
+          instanceOptions[key] = widget.defaults[key];
         }
       }
-      els = document.querySelectorAll(queries.join(','));
-    } else if(!Mutiny.util.isArray(els)) {
-      els = [els];
-    }
-
-    for(var i=0; i < els.length; i++) {
-      var el = els[i];
-      for(name in Mutiny.widgets) {
-        var attr = Mutiny.util.format('data-{0}-{1}', namespace, Mutiny.util.dasherize(name));
-        if(el.hasAttribute(attr)){
-          var data = el.getAttribute(attr);
-          var updatedOptions = initWidget(el, name, data);
-          if(updatedOptions) {
-            el.setAttribute(attr, JSON.stringify(updatedOptions));
-          }
-        }
-      }
+      widget.init(instigator, instanceOptions);
     }
   }
 };
@@ -95,28 +119,3 @@ Mutiny.util.onReady(function(){
     Mutiny.init();
   }
 });
-
-function initWidget(instigator, widgetName, instanceOptions) {
-  var widget = Mutiny.widgets[widgetName];
-  if(widget === undefined) {
-    throw Mutiny.util.format('"{0}" not found', widgetName);
-  }
-
-  try {
-    instanceOptions = instanceOptions ? JSON.parse(instanceOptions) : {};
-  } catch(e) {
-    e.message = Mutiny.util.format('"{0}" cannot parse "{1}"', widgetName, instanceOptions);
-    throw e;
-  }
-
-  if(!instanceOptions.called) {
-    for(var key in widget.defaults) {
-      if(widget.defaults.hasOwnProperty(key) && !instanceOptions.hasOwnProperty(key)) {
-        instanceOptions[key] = widget.defaults[key];
-      }
-    }
-    widget.init(instigator, instanceOptions);
-    instanceOptions.called = true;
-    return instanceOptions;
-  }
-}
