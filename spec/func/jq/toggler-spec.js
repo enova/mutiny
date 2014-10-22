7f58cb8a853f35945e2c7f1b4af45f34ef18f517
fengb <contact@fengb.info>
2014-10-21 22:03:04 -0500

    Cleaner toggler tests

--- spec/func/jq/toggler-spec.js
+++ spec/func/jq/toggler-spec.js
@@ -26,46 +26,53 @@ describe('Mutiny.widgets.jqToggler', function() {
   }
 
   it('triggers basic toggler', function() {
-    expectClass($('#basic-toggler [data-mutiny-jq-toggler]'), '+inactive -active');
-    $('#basic-toggler [data-mutiny-jq-toggler]').click();
-    expectClass($('#basic-toggler [data-mutiny-jq-toggler]'), '+active -inactive');
-    $('#basic-toggler [data-mutiny-jq-toggler]').click();
-    expectClass($('#basic-toggler [data-mutiny-jq-toggler]'), '+inactive -active');
+    var $toggler = $('#basic-toggler [data-mutiny-jq-toggler]');
+    expectClass($toggler, '+inactive -active');
+    $toggler.click();
+    expectClass($toggler, '+active -inactive');
+    $toggler.click();
+    expectClass($toggler, '+inactive -active');
   });
 
   it('triggers Targeted Class toggler', function() {
-    expectClass($('#targeted-class .target'), '+first -last');
-    $('#targeted-class [data-mutiny-jq-toggler]').click();
-    expectClass($('#targeted-class .target'), '+last -first');
-    $('#targeted-class [data-mutiny-jq-toggler]').click();
-    expectClass($('#targeted-class .target'), '+first -last');
+    var $target = $('#targeted-class .target');
+    var $toggler = $('#targeted-class [data-mutiny-jq-toggler]');
+    expectClass($target, '+first -last');
+    $toggler.click();
+    expectClass($target, '+last -first');
+    $toggler.click();
+    expectClass($target, '+first -last');
   });
 
   it('triggers Class Name toggler', function() {
-    expectClass($('#class-name [data-mutiny-jq-toggler]'), '+start -end');
-    $('#class-name [data-mutiny-jq-toggler]').click();
-    expectClass($('#class-name [data-mutiny-jq-toggler]'), '+end -start');
-    $('#class-name [data-mutiny-jq-toggler]').click();
-    expectClass($('#class-name [data-mutiny-jq-toggler]'), '+start -end');
+    var $toggler = $('#class-name [data-mutiny-jq-toggler]');
+    expectClass($toggler, '+start -end');
+    $toggler.click();
+    expectClass($toggler, '+end -start');
+    $toggler.click();
+    expectClass($toggler, '+start -end');
   });
 
   it('triggers Style Changes toggler', function() {
-    expect($('#style-changes [data-mutiny-jq-toggler]')).not.to.have.css('font-style', 'italic');
-    $('#style-changes [data-mutiny-jq-toggler]').click();
-    expect($('#style-changes [data-mutiny-jq-toggler]')).to.have.css('font-style', 'italic');
-    $('#style-changes [data-mutiny-jq-toggler]').click();
-    expect($('#style-changes [data-mutiny-jq-toggler]')).not.to.have.css('font-style', 'italic');
+    var $toggler = $('#style-changes [data-mutiny-jq-toggler]');
+    expect($toggler).to.not.have.css('font-style', 'italic');
+    $toggler.click();
+    expect($toggler).to.have.css('font-style', 'italic');
+    $toggler.click();
+    expect($toggler).to.not.have.css('font-style', 'italic');
   });
 
   it('triggers Checkbox toggler', function() {
-    expectClass($('#checkbox [data-mutiny-jq-toggler]'), '+inactive -active');
-    $('#checkbox [data-mutiny-jq-toggler]').click();
-    expectClass($('#checkbox [data-mutiny-jq-toggler]'), '+active -inactive');
+    var $toggler = $('#checkbox [data-mutiny-jq-toggler]');
+    expectClass($toggler, '+inactive -active');
+    $toggler.click();
+    expectClass($toggler, '+active -inactive');
   });
 
   it('triggers Radio Button toggler', function() {
-    expectClass($('#radio-buttons [data-mutiny-jq-toggler]'), '+inactive -active');
-    $('#radio-buttons [data-mutiny-jq-toggler]').click();
-    expectClass($('#radio-buttons [data-mutiny-jq-toggler]'), '+active -inactive');
+    var $toggler = $('#radio-buttons [data-mutiny-jq-toggler]');
+    expectClass($toggler, '+inactive -active');
+    $toggler.click();
+    expectClass($toggler, '+active -inactive');
   });
 });
