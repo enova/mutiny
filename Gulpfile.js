b96404edaf82e343fb339a72ac2f1827688c9a3f
fengb <contact@fengb.info>
2014-10-21 22:29:35 -0500

    gulp-mocha-phantom

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var mochaPhantom = require('gulp-mocha-phantomjs');
 
 gulp.task('lint', function(){
   return gulp.src(['./Gulpfile.js', './package.json',
@@ -7,4 +8,11 @@ gulp.task('lint', function(){
                    './spec/**/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+
+gulp.task('test', function () {
+  return gulp
+    .src('mocha.html')
+    .pipe(mochaPhantom());
 });
