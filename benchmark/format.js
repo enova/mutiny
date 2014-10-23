1e38c9bb2bcc1353b5ba20ca9dfefae006bc6ce0
fengb <contact@fengb.info>
2014-10-22 23:03:20 -0500

    Separated benchmark scripts

---
+++ benchmark/format.js
@@ -0,0 +1,54 @@
+BenchmarkPlus({
+  'inline': function(){
+    function format(str) {
+      for(var i=1; i < arguments.length; i++) {
+        var regex = new RegExp("\\{" + (i-1) + "\\}", "gm");
+        str = str.replace(regex, arguments[i]);
+      }
+
+      return str;
+    }
+
+    return function(){
+      format('{0} {1} {2} {3}', 'foo', 'bar', 'baz', 'boo');
+    };
+  }(),
+
+  'precached': function(){
+    var regexes = [];
+    for(var i=0; i < 10; i++){
+      regexes[i] = new RegExp("\\{" + i + "\\}", "gm");
+    }
+    function format(str) {
+      for(var i=1; i < arguments.length; i++) {
+        str = str.replace(regexes[i-1], arguments[i]);
+      }
+
+      return str;
+    }
+
+    return function(){
+      format('{0} {1} {2} {3}', 'foo', 'bar', 'baz', 'boo');
+    }
+  }(),
+
+  'cache on demand': function(){
+    var regexes = [];
+    function format(str) {
+      for(var i=1; i < arguments.length; i++) {
+        var r = i-1;
+        var regex = regexes[r];
+        if(!regex){
+          regex = regexes[r] = new RegExp("\\{" + r + "\\}", "gm");
+        }
+        str = str.replace(regex, arguments[i]);
+      }
+
+      return str;
+    }
+
+    return function(){
+      format('{0} {1} {2} {3}', 'foo', 'bar', 'baz', 'boo');
+    }
+  }()
+});
