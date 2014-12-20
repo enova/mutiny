48eb0408d1634addee65e7bcef4b521ea6c8dada
fengb <contact@fengb.info>
2014-12-20 10:40:16 -0600

    Ajax replace onLoad

--- src/jq/ajax-replace.js
+++ src/jq/ajax-replace.js
@@ -4,6 +4,7 @@ Mutiny.widgets.jqAjaxReplace = {
   defaults: {
     loadingClass: 'loading',
     errorClass: 'error',
+    onLoad: Mutiny.init
   },
 
   init: function(instigator, options){
@@ -12,12 +13,13 @@ Mutiny.widgets.jqAjaxReplace = {
     options.success = function(data, textStatus, xhr){
       $instigator.removeClass(options.loadingClass);
       $instigator.html(data);
-      Mutiny.init();
+      options.onLoad();
     };
 
     options.error = function(xhr, textStatus, errorThrown){
       $instigator.removeClass(options.loadingClass).addClass(options.errorClass);
       $instigator.html(xhr.responseText);
+      options.onLoad();
     };
 
     $instigator.addClass(options.loadingClass);
